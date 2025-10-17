8a095c66fc780c073f0bb6131bd586fba6d050ef
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-10-17 16:10:55 +0300

    use fortywo

--- packages/luna-seven/index.ts
+++ packages/luna-seven/index.ts
@@ -1,5 +1,6 @@
 import type { ConfigType } from '@plone/registry';
 
 export default function install(config: ConfigType) {
+  config.settings.fortytwo.voltoUrl = 'https://volto.lunaspace.dev';
   return config;
 }
